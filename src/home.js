9c968b7fd75ac4cc91b3e7b977000cafe614bd8e
ScottGrieveWeb <scottgrieveweb@gmail.com>
2024-11-30 21:18:09 +0000

    Added images folder and syle.css

--- src/home.js
+++ src/home.js
@@ -1,6 +1,12 @@
+import restaurantImage from "./images/restaurant.jpg";
+
 export const homePage = (function () {
     function pageLoad(id){
         const contentDiv = document.getElementById(id);
+
+        const mainImage = document.createElement('img');
+        mainImage.src = restaurantImage;
+        mainImage.setAttribute("id", "restaurant-img");
 
         const headLine = document.createElement("h1");
         const headLineNode = document.createTextNode("My Restaurant");
@@ -10,6 +16,7 @@ export const homePage = (function () {
         const tagLineNode = document.createTextNode("The best restaurant on the web!");
         tagLine.appendChild(tagLineNode);
 
+        contentDiv.appendChild(mainImage);
         contentDiv.appendChild(headLine);
         contentDiv.appendChild(tagLine);
     }
